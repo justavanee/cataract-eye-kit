d92da5717b2759ecf9451c5561413916d47beb6a
justavanee <avaneelakare@gmail.com>
2025-04-29 08:32:08 +0530

    small change

--- app.py
+++ app.py
@@ -81,5 +81,6 @@ def predict():
     return jsonify({'result': result})
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, port=5000)
